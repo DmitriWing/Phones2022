0b1ff39ea28e223c99deafa3f413855c9f3f867f
DmitProg <dmonk30@gmail.com>
2022-09-28 11:16:32 +0300

    trying to add roles in db

--- Phones2022/Controllers/AccountController.cs
+++ Phones2022/Controllers/AccountController.cs
@@ -75,7 +75,7 @@ namespace Phones2022.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -392,7 +392,7 @@ namespace Phones2022.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Phones");
         }
 
         //
@@ -449,7 +449,7 @@ namespace Phones2022.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Phones");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
